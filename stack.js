042b865013b3e167cfc215de2342f0faab8572ea
johnny lamb <agnusjack@gmail.com>
2018-07-21 21:04:53 -0600

    updating stack implementation

--- stack.js
+++ stack.js
@@ -1,44 +1,45 @@
 // implement a STACK data structure in Javascript. First implement with built in array methods. Second, implement with custom mySet constructor.
 
-// STACKS are described as a FIRST IN LAST OUT data strucutre. A good analogy/example of a stack is a pile(stack) of books. Imagine you are holding 3 books in your arms.
-// Now place the 1st book down on the table. 
-// This is your FIRST IN,
-// now place the 2nd book on top of the 1st, and the 3rd on top of the second. Your 3rd is the first book that will be taken off the pile. then your second, then your 1st. 
-// This is why it is called FIRST IN LAST OUT, because the 1st book you put down is the last book you pick up.
-// A common STACK you come across in the digital world is your internet browsing history.
-// Imagine you open your browser and you search for "best brewery near me", then you click on the top result, and then you click a link within the site you were brought to. 
-// your browsing history "STACK" would look something like this, ['www.google.com','google.com/searchresultsforbrewerybearme','www.greatdivide.com','www.greatdivide.com/beerlist']
+/*STACKS are described as a FIRST IN LAST OUT data strucutre. A good analogy/example of a stack is a pile(stack) of books. Imagine you are holding 3 books in your arms.
+Now place the 1st book down on the table. 
+This is your FIRST IN,
+now place the 2nd book on top of the 1st, and the 3rd on top of the second. Your 3rd is the first book that will be taken off the pile. then your second, then your 1st. 
+This is why it is called FIRST IN LAST OUT, because the 1st book you put down is the last book you pick up.
+A common STACK you come across in the digital world is your internet browsing history.
+Imagine you open your browser and you search for "best brewery near me", then you click on the top result, and then you click a link within the site you were brought to. 
+your browsing history "STACK" would look something like this, ['www.google.com','google.com/searchresultsforbrewerybearme','www.greatdivide.com','www.greatdivide.com/beerlist']
+
+common functions for a STACK are: 
+push -- push the latest element onto the end/top of the STACK
+pop -- pop the latest element off of the end/top of the STACK
+peek -- peek at or see the latest element at the end/top of the STACK
+length -- check the amount of elements in the STACK
+
+*/
 
 
-// functions: push, pop, peek, length
+const browsingHistory = []; // this is our stack
 
-var letters = []; // this is our stack
 
-var word = "testing"
-
-var rword = "";
-
-// put letters of word into stack
-for (var i = 0; i < word.length; i++) {
-   letters.push(word[i]);
+// put/push elements into a stack
+function push(arr,item){
+    
+        arr.push(item);
+    
+     return arr;
 }
 
-// pop off the stack in reverse order
-for (var i = 0; i < word.length; i++) {
-   rword += letters.pop(); 
-}
-
-if (rword === word) {
-   console.log(word + " is a palindrome.");
-}
-else {
-   console.log(word + " is not a palindrome.");
+function pop(arr){
+// pop elements off the stack starting from the top/end
+        arr.pop(); 
+    return arr;
+    
 }
 
 
 
 // Creates a stack
-var Stack = function() {
+const Stack = function() {
     this.count = 0;
     this.storage = {};
   
@@ -55,7 +56,7 @@ var Stack = function() {
         }
 
         this.count--;
-        var result = this.storage[this.count];
+        let result = this.storage[this.count];
         delete this.storage[this.count];
         return result;
     }
