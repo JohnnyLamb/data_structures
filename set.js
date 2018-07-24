2cbf2735b5c6575a95dad285c32b9a6ef487a8e4
johnny lamb <agnusjack@gmail.com>
2018-07-24 00:00:54 -0600

    set rewrite

--- set.js
+++ set.js
@@ -1,37 +1,45 @@
 /* Sets */
+function customSet(){
+    // an array will hold the elements in the set
+    let collection = [];
+
+    // the "has" method will check if the given set has an element or not, return true if if does and false if it does not
+    this.has = function(element){
+        return (collection.indexOf(element)!== -1);
+    }
+    // this method will return all the elements in the set
+    this.values = function(){
+        return collection;
+    }
+    // this method adds and element to a given set
+    this.add = function(element){
+        if(!this.has(element)){
+            collection.push(element)
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    // this method deletes an element from the set
+    this.deleteElement = function(element){
+        if(this.has(element)){
+            index = collection.indexOf(element)
+            collection.splice(index,1)
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // this method returns the size of the collection
+    this.size = function(){
+        return collection.length;
+    }
+
+}
 
 function customSet() {
-    // the let collection will hold the set
-    let collection = [];
-    // this method will check for the presence of an element and return true or false
-    this.has = function(element) {
-        return (collection.indexOf(element) !== -1);
-    };
-    // this method will return all the values in the set
-    this.values = function() {
-        return collection;
-    };
-    // this method will add an element to the set
-    this.add = function(element) {
-        if(!this.has(element)){
-            collection.push(element);
-            return true;
-        }
-        return false;
-    };
-    // this method will remove an element from a set
-    this.remove = function(element) {
-        if(this.has(element)){
-            index = collection.indexOf(element);
-            collection.splice(index,1);
-            return true;
-        }
-        return false;
-    };
-    // this method will return the size of the collection
-    this.size = function() {
-        return collection.length;
-    };
+ 
     // this method will return the union of two sets
     this.union = function(otherSet) {
         let unionSet = new customSet();
