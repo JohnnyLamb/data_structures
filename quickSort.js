c5d15d7b965ac28b61cbc7c71eb5290b622c86e4
johnny lamb <agnusjack@gmail.com>
2018-07-29 22:46:33 -0600

    added quicksort algorithm

---
+++ quickSort.js
@@ -0,0 +1,19 @@
+let array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+function quickSort(array){
+    if(array.length < 2){
+        return array
+    }
+        let pivot = array[0];
+        let lesser = [];
+        let greater = [];
+
+        for(let i = 1; i < array.length; i++){
+            if(array[i] < pivot){
+                lesser.push(array[i])
+            }else{
+                greater.push(array[i])
+            }
+        }
+     return quickSort(lesser).concat(pivot, quickSort(greater));
+}
+console.log(quickSort(array))
