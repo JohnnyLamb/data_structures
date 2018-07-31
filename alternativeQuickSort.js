b965c8fd779f7a87afe8f525106b268b1d7b68d0
Johnny Lamb <JOLAMB@davita.corp>
2018-07-31 17:53:03 -0600

    an alternative quicksort

---
+++ alternativeQuickSort.js
@@ -0,0 +1,31 @@
+
+var myArray = [3, 0, 232, 5, -1, 4 ];
+const quick_Sort = (arr) => {
+ 
+  let less=[]
+  let pivotList = []
+  let more = []
+
+  if(arr.length < 2){
+    return arr
+  } else {
+    pivot = arr[0]
+    for(let i=0;i<arr.length;i++){
+      if (arr[i] < pivot){
+        less.push(arr[i])
+      }
+      else if(arr[i] > pivot){
+        more.push(arr[i])
+      }
+      else{
+        pivotList.push(arr[i])
+      }
+    }
+    
+   return quick_Sort(less).concat(pivotList).concat(quick_Sort(more));
+  }
+}
+
+console.log("Original array: " + myArray);
+var sortedArray = quick_Sort(myArray);
+console.log("Sorted array: " + sortedArray);
